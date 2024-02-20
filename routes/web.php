914a0c2396060cc5832bc3d276284e9a164dff78
SamyAlinne <117871370+SamyAlinne@users.noreply.github.com>
2024-02-20 10:40:03 -0600

    crud

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ $router->get('/', function () use ($router) {
 });
 
 $router->get('users','UsersController@index');
+$router->get('users/{id}','UsersController@show');
+$router->post('users','UsersController@store');
+$router->put('users/{id}','UsersController@update');
+$router->delete('users/{id}','UsersController@destroy');
