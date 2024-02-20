15be98ac1d34ece8a3aa7367ad18152ec6f3bb1d
SamyAlinne <117871370+SamyAlinne@users.noreply.github.com>
2024-02-20 10:40:08 -0600

    s

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -17,4 +17,25 @@ class UsersController extends Controller
     public function show($id) {
         return User::find($id);
     }
+
+    //crear un usuario
+    public function store(Request $request){
+        return User::create($request->all());
+    }
+
+    //actualizar un usuario
+    public function update(Request $request, $id){
+        $user = User::find($id);
+        $user->update($request->all());
+        $user->save();
+        return $user;
+    }
+
+    //eliminar un usuario
+    public function destroy($id){
+        $user = User::find($id);
+        $user->delete();
+        return $user;
+    }
+
 }
